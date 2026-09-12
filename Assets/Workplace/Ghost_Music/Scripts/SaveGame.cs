c4e6e48c6b9b7e62e138ce6a492d64c630d2c70a
GhostMusic25 <gretchen.baune@gmail.com>
2026-09-11 22:12:11 -0500

    Alpha_Sprint
    MainArea Showcase blockout
    Added changes to save system for stamina and health through levels

--- Assets/Workplace/Ghost_Music/Scripts/SaveGame.cs
+++ Assets/Workplace/Ghost_Music/Scripts/SaveGame.cs
@@ -1,15 +1,24 @@
 using UnityEngine;
-using System; 
+using System;
+using NUnit.Framework;
+using System.Collections.Generic; 
 
-public class SaveGame : MonoBehaviour
+public class SaveGame
 {
     public float playerPosX;
     public float playerPosY;
     public float playerPosZ;
 
     public float playerHealth;
+    public float playerStamina;
+    public float playerMana;
 
+    public List<ItemSaveData> inventory = new List<ItemSaveData>();
     public string sceneName; 
 
-    
+    public class ItemSaveData
+    {
+        public string itemID;
+        public int quantity; 
+    }
 }
