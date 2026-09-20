a1a645216ad12a8b8612f6063c1f36b4bf303f46
Tr1Tangle <tay.porath@gmail.com>
2026-09-20 16:03:41 -0600

    Bug fix for camera and inventory locked barriers and keys
    Did some bug fixes on the camera, added in the keys on the bosses as a spawn when they beat the boss. Made it so that inventory system automatically pops out of UI prefab so we persist with current equipment and did the same for equipped items.

--- Assets/Workplace/TayTrain/scripts/Managers/EquipmentManager.cs
+++ Assets/Workplace/TayTrain/scripts/Managers/EquipmentManager.cs
@@ -34,6 +34,8 @@ public class EquipmentManager : MonoBehaviour, IEquipmentPickup
         Boots
     }
 
+  
+
     public EquipmentData GetEquipment(EquipmentData newEquipment)
     {
         if (newEquipment == null)
@@ -91,6 +93,12 @@ public class EquipmentManager : MonoBehaviour, IEquipmentPickup
         }
 
         newEquipment.Equip(stats);
+
+        if(InventorySystem.Instance != null)
+        {
+            InventorySystem.Instance.SetEquippedItem(newEquipment.slot, newEquipment);
+        }
+
         UpdateEquipmentVisual(newEquipment);
         Debug.Log("Equipped: " + newEquipment.itemName);
         return oldEquipment;
@@ -127,6 +135,11 @@ public class EquipmentManager : MonoBehaviour, IEquipmentPickup
         {
             equipmentToRemove.Unequip(stats);
             RemoveEquipmentVisual(slot);
+
+            if(InventorySystem.Instance != null)
+            {
+                InventorySystem.Instance.SetEquippedItem(slot, null);
+            }
         }
         
         return equipmentToRemove;
@@ -272,5 +285,52 @@ public class EquipmentManager : MonoBehaviour, IEquipmentPickup
                 }
                 break;
         }
+    }
+
+    private void Start()
+    {
+        RestoreEquipment();
+    }
+
+    private void RestoreEquipment()
+    {
+        if (InventorySystem.Instance == null)
+            return;
+
+        RestoreSlot(InventorySystem.Instance.GetEquippedItem(EquipmentSlot.Helmet));
+
+        RestoreSlot(InventorySystem.Instance.GetEquippedItem(EquipmentSlot.Amulet));
+
+        RestoreSlot(InventorySystem.Instance.GetEquippedItem(EquipmentSlot.Armor));
+
+        RestoreSlot(InventorySystem.Instance.GetEquippedItem(EquipmentSlot.Boots));
+    }
+
+    private void RestoreSlot(EquipmentData equipment)
+    {
+        if (equipment == null)
+            return;
+
+        switch(equipment.slot)
+        {
+            case EquipmentSlot.Helmet: 
+                helmet = equipment;
+                break;
+
+            case EquipmentSlot.Amulet:
+                amulet = equipment;
+                break;
+
+            case EquipmentSlot.Armor:
+                armor = equipment;
+                break;
+
+            case EquipmentSlot.Boots:
+               boots  = equipment;
+                break;
+        }
+
+        equipment.Equip(stats);
+        UpdateEquipmentVisual(equipment);
     }
 }
